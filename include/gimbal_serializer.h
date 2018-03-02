fe3257fa0307ac900fe6e7674bcffcee2515a084
dallinbriggs <dallinbriggs@gmail.com>
2018-03-01 18:08:32 -0700

    Got std bind working

--- include/gimbal_serializer.h
+++ include/gimbal_serializer.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Vector3Stamped.h>
+#include <functional>
 
 #include "async_comm/serial.h"
 
@@ -15,11 +16,33 @@ class GimbalSerializer
 {
 public:
     GimbalSerializer();
+
 private:
     ros::NodeHandle nh_;
+    ros::NodeHandle nh_private_;
+
     geometry_msgs::Vector3Stamped gimbal_command;
     ros::Subscriber command_sub;
+    ros::Publisher command_echo;
+
+    //Variables
+    float x_command;
+    float y_command;
+    float z_command;
+
+    // Params
+    std::string port_;
+    int baudrate_;
+
+    // Callbacks and functions
     void command_callback(const geometry_msgs::Vector3StampedConstPtr &msg);
+    void serialize_msg(float x, float y, float z);
+    void init_serial();
+    void serial_receive(uint8_t byte);
+
+    // Serialization
+    async_comm::Serial *serial_;
+
 };
 
 #endif // GIMBAL_SERIALIZER_H
--- src/gimbal_serializer.cpp
+++ src/gimbal_serializer.cpp
@@ -4,12 +4,55 @@ namespace gimbal_serializer{
 
 
 GimbalSerializer::GimbalSerializer():
-    nh_(ros::NodeHandle())
+    nh_(ros::NodeHandle()),
+    nh_private_(ros::NodeHandle("~"))
+{
+    // Set/get params
+    nh_private_.param<std::string>("port", port_, "/dev/ttyACM0");
+    nh_private_.param<int>("baudrate", baudrate_, 115200);
+
+    // Initialize serial stuff
+    init_serial();
+
+
+    // Setup ros subscribers and publishers
+    command_sub = nh_.subscribe("gimbal/command", 1, &GimbalSerializer::command_callback, this);
+    command_echo = nh_.advertise<geometry_msgs::Vector3Stamped>("gimbal/current", 1);
+}
+
+void GimbalSerializer::command_callback(const geometry_msgs::Vector3StampedConstPtr &msg)
+{
+    //    command_echo.publish(msg);
+    x_command = msg->vector.x;
+    y_command = msg->vector.y;
+    z_command = msg->vector.z;
+    serialize_msg(x_command, y_command, z_command);
+}
+
+void GimbalSerializer::serialize_msg(float x, float y, float z)
 {
 
 }
 
+void GimbalSerializer::init_serial()
+{
+    serial_ = new async_comm::Serial(port_, (unsigned int)baudrate_);
+    serial_->register_receive_callback(std::bind(&GimbalSerializer::serial_receive, this, std::placeholders::_1));
+
+    if (!serial_->init())
+    {
+        std::printf("Failed to initialize serial port\n");
+    }
+}
+
+void GimbalSerializer::serial_receive(uint8_t byte)
+{
+    // This is where you define how to handle a received byte.
+}
+
+
 } //end gimbal_serializer namespace
+
 
 int main(int argc, char** argv)
 {
