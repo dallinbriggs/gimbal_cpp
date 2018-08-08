6742d50229e19f656529e9e03eb309a80f2f9e43
dallinbriggs <dallinbriggs@gmail.com>
2018-08-08 12:59:09 -0600

    fixed the test function

--- scripts/gimbal_test.py
+++ scripts/gimbal_test.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Vector3Stamped
 def gimbal_test():
     pub = rospy.Publisher('gimbal/control', Vector3Stamped, queue_size=1)
     rospy.init_node('gimbal_test_node', anonymous=True)
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(330)
     msg = Vector3Stamped()
     msg.header.frame_id = "test"
     msg.header.seq = 0
@@ -16,11 +16,11 @@ def gimbal_test():
 
     while not rospy.is_shutdown():
         msg.header.seq = msg.header.seq + 1
-        if msg.vector.z < 180.0:
-            msg.vector.x = msg.vector.x + 0.0157
-            msg.vector.y = msg.vector.y - 0.0157
-            msg.vector.z = msg.vector.z + 1.0
-        elif msg.vector.z >= 180.0:
+        if msg.vector.z < 3.14:
+            msg.vector.x = msg.vector.x + 0.00157
+            msg.vector.y = msg.vector.y - 0.00157
+            msg.vector.z = msg.vector.z + .00157
+        elif msg.vector.z >= 3.14:
             msg.vector.x = 0.0
             msg.vector.y = 0.0
             msg.vector.z = 0.0
